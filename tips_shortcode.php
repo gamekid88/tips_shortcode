4c940b765c844d05000bafef1e46822725670b19
Frank Corso <fpcorso@gmail.com>
2014-12-16 17:50:22 -0500

    Added Plugin Documentation Code

--- tips_shortcode.php
+++ tips_shortcode.php
@@ -1,4 +1,22 @@
 <?php
+/**
+ * Plugin Name: Tips Shortcode
+ * Plugin URI: http://mylocalwebstop.com
+ * Description: Shortcode to create tip box
+ * Author: Frank Corso
+ * Author URI: http://mylocalwebstop.com
+ * Version: 0.1.0
+ *
+ * Disclaimer of Warranties
+ * The plugin is provided "as is". My Local Webstop and its suppliers and licensors hereby disclaim all warranties of any kind, 
+ * express or implied, including, without limitation, the warranties of merchantability, fitness for a particular purpose and non-infringement. 
+ * Neither My Local Webstop nor its suppliers and licensors, makes any warranty that the plugin will be error free or that access thereto will be continuous or uninterrupted.
+ * You understand that you install, operate, and uninstall the plugin at your own discretion and risk.
+ * 
+ * @author Frank Corso
+ * @version 0.1.0
+ */
+ 
 function tip_block_shortcode( $atts, $content = null ) {
 return "<style>.tipBox {
       color:#555;
@@ -9,5 +27,6 @@ return "<style>.tipBox {
 		border:1px solid #f2c779;
 }</style><div class='tipBox'><span class='dashicons dashicons-lightbulb'></span>$content</div>";
 }
+
 add_shortcode( 'tip', 'tip_block_shortcode' );
 ?>
