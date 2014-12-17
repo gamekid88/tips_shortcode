91026ff87a4dded703676d6c26a64adc67cfd1e9
Frank Corso <fpcorso@gmail.com>
2014-12-16 21:29:06 -0500

    Added Enqueue Style For Dashicons

--- tips_shortcode.php
+++ tips_shortcode.php
@@ -18,14 +18,15 @@
  */
  
 function tip_block_shortcode( $atts, $content = null ) {
-return "<style>.tipBox {
-      color:#555;
-		border-radius:10px;
-		padding:10px 36px;
-		margin:10px;
-    background:#fff8c4;
-		border:1px solid #f2c779;
-}</style><div class='tipBox'><span class='dashicons dashicons-lightbulb'></span>$content</div>";
+	wp_enqueue_style( 'dashicons' );
+	return "<style>.tipBox {
+	      color:#555;
+			border-radius:10px;
+			padding:10px 36px;
+			margin:10px;
+	    background:#fff8c4;
+			border:1px solid #f2c779;
+	}</style><div class='tipBox'><span class='dashicons dashicons-lightbulb'></span>$content</div>";
 }
 
 add_shortcode( 'tip', 'tip_block_shortcode' );
